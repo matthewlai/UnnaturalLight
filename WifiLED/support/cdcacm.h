3439066ab292b36e3d3924eee33896c7e2195377
Matthew Lai <m@matthewlai.ca>
2015-05-09 23:48:45 +0100

    Added CDCADM support

---
+++ WifiLED/support/cdcacm.h
@@ -0,0 +1,16 @@
+#ifndef CDCACM_H
+#define CDCACM_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init_cdcacm(void);
+void poll_cdcacm(void);
+void cdcacm_send(char *data, int size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CDCACM_H
